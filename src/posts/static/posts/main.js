433dc1a33f8fae54f87d60d290b5b45c879a7a31
Mrunal Amin <mami1581@conestogac.on.ca>
2025-04-07 01:19:48 -0400

    first call ajax and got posts objects with ajax

--- src/posts/static/posts/main.js
+++ src/posts/static/posts/main.js
@@ -1,5 +1,36 @@
 console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
+const postsBox = document.getElementById('posts-box')
 
 helloWorldBox.innerHTML = 'hello <b>world</b>'
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url:'/data/',
+    success: function(response){
+        console.log(response)
+        const data = response.data
+        console.log(data)
+        data.forEach(el => {
+            postsBox.innerHTML += `
+              ${el.title} - <b>${el.body}</b><br><br>
+            `
+          });          
+    },
+    error: function(error){
+        console.log( error)
+    }
+})
